fbf880087b4077629aaffae68cb2d210d4bcd7c4
rbbanner <rlfoster2018@gmail.com>
2020-11-09 16:48:42 -0600

    Assignment 4 Part 4 Skills Update

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,6 +1,7 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -10,6 +11,9 @@ public class Skill extends AbstractEntity {
     @NotNull
     @Size(min=1, max=250)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private Job jobs;
 
     public Skill() { }
 
